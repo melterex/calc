0b1fa55c070286cd7c25872a58dee6772c54b052
sktfrt <mdashameldo@gmail.com>
2026-09-14 18:07:42 +0300

    added DB for calculation history

--- server/CalcServer/History.cs
+++ server/CalcServer/History.cs
@@ -1,17 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<HistoryEntry> Calculations { get; set; }
+}
+
 public class History : IHistory
 {
+    private readonly AppDbContext _db;
+
+    public History(AppDbContext db) => _db = db;
+    
     public HistoryEntry SaveToHistory(string expression, double result)
     {
-        throw new NotImplementedException();
+        var entry = new HistoryEntry(0, expression, result);
+
+        _db.Calculations.Add(entry);
+        _db.SaveChanges();
+
+        return entry;
     }
 
-    public IEnumerable<HistoryEntry> GetAllHistory() 
-    {
-        throw new NotImplementedException();
-    }
-
-    public HistoryEntry? GetHistoryById(int id)
-    {
-        throw new NotImplementedException();
-    }
+    public IEnumerable<HistoryEntry> GetAllHistory() => _db.Calculations.ToList();
+    public HistoryEntry? GetHistoryById(int id) => _db.Calculations.Find(id);
 }
